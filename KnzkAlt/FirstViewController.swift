bcef96ac757805895f2f917c5b44d981022f6af4
mopopo <wtm4080@atcurio.com>
2017-10-30 00:45:53 +0900

    Initial commit.

---
+++ KnzkAlt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  KnzkAlt
+//
+//  Created by mopopo on 2017/10/30.
+//  Copyright © 2017年 AtCurio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KnzkAlt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KnzkAlt
+//
+//  Created by mopopo on 2017/10/30.
+//  Copyright © 2017年 AtCurio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
