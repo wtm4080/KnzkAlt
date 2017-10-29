55c9810d849a5fcfb5957779682cd1607d929537
mopopo <wtm4080@atcurio.com>
2017-10-30 02:14:26 +0900

    Add login navigation view controllers.

---
+++ KnzkAlt/InitialVC.swift
@@ -0,0 +1,12 @@
+//
+//  InitialVC.swift
+//  KnzkAlt
+//
+//  Created by mopopo on 2017/10/30.
+//  Copyright © 2017年 AtCurio. All rights reserved.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+}
---
+++ KnzkAlt/LoginVC.swift
@@ -0,0 +1,12 @@
+//
+//  LoginVC.swift
+//  KnzkAlt
+//
+//  Created by mopopo on 2017/10/30.
+//  Copyright © 2017年 AtCurio. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+}
