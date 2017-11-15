bdf58b1cb702ca42836612c3991789c039f7a8d5
mopopo <wtm4080@atcurio.com>
2017-11-15 12:08:18 +0900

    Add timelines switch buttons state update.

--- KnzkAlt/TimelineSwitch.swift
+++ KnzkAlt/TimelineSwitch.swift
@@ -15,32 +15,57 @@ class TimelineSwitch: UIView {
 
 class TimelineSwitchOwner: NibViewOwner<TimelineSwitch> {
     private var _currentTLKind = TLKind.home
+    
+    @IBOutlet weak private var homeButton: UIButton!
+    @IBOutlet weak private var localButton: UIButton!
+    @IBOutlet weak private var federationButton: UIButton!
 
+    override init() {
+        super.init()
+
+        _updateButtonStates()
+    }
+    
     var currentTLKind: TLKind {
         return _currentTLKind
     }
 
     @IBAction func homeAction(_ sender: Any) {
-        _currentTLKind = .home
-
-        Notifications.switchTL.post(
-                tlParams: TLParams(kind: .home, pos: .unspecified)
-        )
+        _buttonAction(tlKind: .home)
     }
     
     @IBAction func localAction(_ sender: Any) {
-        _currentTLKind = .local
-
-        Notifications.switchTL.post(
-                tlParams: TLParams(kind: .local, pos: .unspecified)
-        )
+        _buttonAction(tlKind: .local)
     }
     
     @IBAction func federationAction(_ sender: Any) {
-        _currentTLKind = .federation
+        _buttonAction(tlKind: .federation)
+    }
+
+    private func _buttonAction(tlKind: TLKind) {
+        _currentTLKind = tlKind
+
+        _updateButtonStates()
 
         Notifications.switchTL.post(
-                tlParams: TLParams(kind: .federation, pos: .unspecified)
+                tlParams: TLParams(kind: tlKind, pos: .unspecified)
         )
+    }
+
+    private func _updateButtonStates() {
+        switch _currentTLKind {
+            case .home:
+                homeButton.isSelected = true
+                localButton.isSelected = false
+                federationButton.isSelected = false
+            case .local:
+                homeButton.isSelected = false
+                localButton.isSelected = true
+                federationButton.isSelected = false
+            case .federation:
+                homeButton.isSelected = false
+                localButton.isSelected = false
+                federationButton.isSelected = true
+        }
     }
 }
