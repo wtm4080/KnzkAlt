de5d1e946e4108c912d8d55853d09bd7e9729853
mopopo <wtm4080@atcurio.com>
2018-01-25 13:07:14 +0900

    Create AppNotification.

---
+++ KnzkAlt/AppNotification/AppNotification.swift
@@ -0,0 +1,11 @@
+//
+// Created by mopopo on 2018/01/25.
+// Copyright (c) 2018 AtCurio. All rights reserved.
+//
+
+import Foundation
+
+struct AppNotification {
+    static let shared = AppNotification()
+    private init () {}
+}
