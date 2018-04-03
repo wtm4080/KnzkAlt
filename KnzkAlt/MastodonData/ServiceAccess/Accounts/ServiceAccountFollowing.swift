f078398c40cd9ece23475f8eb46d43caad8428f2
mopopo <wtm4080@atcurio.com>
2018-04-03 17:08:57 +0900

    Add ServiceAccountFollowing.

---
+++ KnzkAlt/MastodonData/ServiceAccess/Accounts/ServiceAccountFollowing.swift
@@ -0,0 +1,21 @@
+//
+// Created by mopopo on 2018/02/08.
+// Copyright (c) 2018 AtCurio. All rights reserved.
+//
+
+import Moya
+
+struct ServiceAccountFollowing: TargetType {
+    let baseURL: URL
+
+    let id: String
+    let max_id: Int?
+    let since_id: Int?
+    let limit: Int?
+
+    var path: String { return  "/api/v1/accounts/\(id)/following" }
+    let method: Moya.Method = .get
+    let task: Task = .requestPlain
+    let sampleData = "".data(using: .utf8)
+    let headers: [String: String]? = nil
+}
