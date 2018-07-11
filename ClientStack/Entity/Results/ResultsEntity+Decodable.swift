9fe3dc2eba6f8c0606def36757b3b902b0808749
mopopo <wtm4080@atcurio.com>
2018-07-11 21:41:22 +0900

    Add JSON decoding for results entity.

---
+++ ClientStack/Entity/Results/ResultsEntity+Decodable.swift
@@ -0,0 +1,22 @@
+//
+// Created by mopopo on 2018/07/11.
+// Copyright (c) 2018 AtCurio. All rights reserved.
+//
+
+import Foundation
+
+extension ResultsEntity: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case accounts
+        case statuses
+        case hashtags
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        accounts = try values.decode(Array<AccountEntityCompound>.self, forKey: .accounts)
+        statuses = try values.decode(Array<StatusCompoundEntity>.self, forKey: .statuses)
+        hashtags = try values.decode(Array<String>.self, forKey: .hashtags)
+    }
+}
--- ClientStack/Entity/Results/ResultsEntity.swift
+++ ClientStack/Entity/Results/ResultsEntity.swift
@@ -5,8 +5,14 @@
 
 import Foundation
 
-struct ResultsEntity: Decodable {
+/// Search (or some other operations) results entity
+struct ResultsEntity {
+    /// An array of matched Accounts
     let accounts: [AccountEntityCompound]
+
+    /// An array of matched Statuses
     let statuses: [StatusCompoundEntity]
+
+    /// An array of matched hashtags, as strings
     let hashtags: [String]
 }
